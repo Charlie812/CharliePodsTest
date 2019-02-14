cfe3ff710480d5ba8c736c787ce9906864089757
CharlieChen <chaocheng812@gmail.com>
2019-02-14 15:18:29 +0800

    Fisrt

---
+++ CharlieFramework/CharlieFramework.framework/Headers/CharlieFramework.h
@@ -0,0 +1,19 @@
+//
+//  CharlieFramework.h
+//  CharlieFramework
+//
+//  Created by Charlie Chen on 2018/10/30.
+//  Copyright © 2018年 CSI Technology group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CharlieFramework.
+FOUNDATION_EXPORT double CharlieFrameworkVersionNumber;
+
+//! Project version string for CharlieFramework.
+FOUNDATION_EXPORT const unsigned char CharlieFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CharlieFramework/PublicHeader.h>
+
+
